5dde4ffdd4498fe896419aeb4f6051921ac8cef8
IslamelGebaly <islamlang@gmail.com>
2022-11-12 22:53:19 +0200

    Filled PointSET

--- KdTree.java
+++ KdTree.java
@@ -5,27 +5,93 @@ import java.util.LinkedList;
 
 public class KdTree {
 
-    public KdTree() { // construct an empty set of points
+    private class TreeNode {
+
+        Point2D p;
+        TreeNode leftNode;
+        TreeNode rightNode;
+        Boolean vertical;
+
+        TreeNode(Point2D p) {
+            this.p = p;
+            this.leftNode = null;
+            this.rightNode = null;
+            this.vertical = true;
+        }
+
+        private TreeNode(Point2D p, boolean vertical) {
+            this.p = p;
+            this.leftNode = null;
+            this.rightNode = null;
+            this.vertical = vertical;
+        }
+
+        void insertLeft(Point2D p) {
+            if (vertical)
+                this.leftNode = new TreeNode(p, false);
+            else
+                this.leftNode = new TreeNode(p, true);
+        }
+
+        void insertRight(Point2D p) {
+            if (vertical)
+                this.rightNode = new TreeNode(p, false);
+            else
+                this.rightNode = new TreeNode(p, true);
+        }
+
+        TreeNode traverseLeft() {
+            if (this.leftNode == null)
+                return null;
+
+            return this.leftNode;
+        }
+
+        TreeNode traverseRight() {
+            if (this.rightNode == null)
+                return null;
+
+            return this.rightNode;
+        }
+
+        Point2D getP() {
+            return this.p;
+        }
+
+    }
+
+    private TreeNode root;
+    private int size;
+
+    public KdTree() {
+        root = null;
+        size = 0;
     }
 
     public boolean isEmpty() {  // is the set empty?
-        return false;
+        return !(root == null);
     }
 
     public int size() {  // number of points in the set
-        return 0;
+        return this.size;
     }
 
     public void insert(Point2D p) { // add the point to the set (if it is not already in the set)
-        return;
+        if (root == null)
+            root = new TreeNode(p);
+        else if (root.getP().x() < p.x()) {
+
+        }
     }
 
     public boolean contains(Point2D p) { // does the set contain point p?
-        return false;
+        return set.contains(p);
     }
 
-    public void draw() { // draw all points to standard draw
-        return;
+    public void draw() // draw all points to standard draw
+    {
+        for (Point2D p : set)
+            p.draw();
     }
 
     public Iterable<Point2D> range(RectHV rect) { // all points that are inside the rectangle (or on the boundary)
