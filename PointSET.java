efa2598d566dbedeb4dc3c9e47ddccc1a957344d
IslamelGebaly <islamlang@gmail.com>
2022-11-13 17:01:14 +0200

    Dealt with corner cases

--- PointSET.java
+++ PointSET.java
@@ -3,13 +3,15 @@ import edu.princeton.cs.algs4.RectHV;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.TreeSet;
 
 public class PointSET {
     // construct an empty set of points
-    private List<Point2D> set;
+    private TreeSet<Point2D> set;
+
 
     public PointSET() {
-        set = new ArrayList<>();
+        set = new TreeSet<>();
     }
 
     public boolean isEmpty() {  // is the set empty?
@@ -20,21 +22,32 @@ public class PointSET {
         return set.size();
     }
 
-    public void insert(Point2D p) { // add the point to the set (if it is not already in the set)
+    public void insert(Point2D p) {// add the point to the set (if it is not already in the set)
+        if (p == null)
+            throw new IllegalArgumentException();
         set.add(p);
     }
 
     public boolean contains(Point2D p) { // does the set contain point p?
+        if (p == null)
+            throw new IllegalArgumentException();
+
         return set.contains(p);
     }
 
     public void draw() // draw all points to standard draw
     {
+        if (isEmpty())
+            return;
+
         for (Point2D p : set)
             p.draw();
     }
 
-    public Iterable<Point2D> range(RectHV rect) { // all points that are inside the rectangle (or on the boundary)
+    public Iterable<Point2D> range(RectHV rect) {// all points that are inside the rectangle (or on the boundary)
+        if (rect == null)
+            throw new IllegalArgumentException();
+
         List<Point2D> inRangePoints = new ArrayList<Point2D>();
 
         for (Point2D p : set) {
@@ -46,6 +59,9 @@ public class PointSET {
     }
 
     public Point2D nearest(Point2D p) {
+        if (p == null)
+            throw new IllegalArgumentException();
+
         if (isEmpty())
             return null;
 
